0cf1af7c271d334eeced8f827785dd42ce834059
konay812020 <konay812020@gmail.com>
2022-08-03 20:22:55 +0630

    dom_window

---
+++ js/Window.js
@@ -0,0 +1,25 @@
+// alert,prompt,confirm,etc...
+//window > outerHeight,innerHeight,scrollX/Y
+//window.location.search/href/reload
+
+let name = "Nay Oo Lwin";
+// window.alert(name);
+
+// let val = window.prompt("What is your name");
+// console.log(val.valueOf());
+
+// let val2 = window.confirm("Are you Okay?");
+// console.log(val2);
+
+// if (val2 == true) {
+//   console.log("You are the best");
+// } else {
+//   console.log("Be gone Be By Gone");
+// }
+console.log(window.outerHeight);
+console.log(window.innerHeight);
+console.log(window.outerWidth);
+console.log(window.innerWidth);
+console.log(window.scrollY);
+
+// console.log(window.location.reload());
